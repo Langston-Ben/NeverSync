be55fbe6bf0b42c10c1e087820b5df21d944a599
benjaminlangston <benjaminlangston@10.0.0.80>
2018-10-30 00:02:47 -0500

    This is completed up to page 17.  Start with "Implementing the savePlayer() method tomorrow.

--- NeverSync/src/byui/cit260/neversync/view/StartProgramView.java
+++ NeverSync/src/byui/cit260/neversync/view/StartProgramView.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package byui.cit260.neversync.view;
+import cit260.neversync.model.Player;
 import java.util.Scanner;
 
 /**
@@ -59,20 +60,61 @@ public class StartProgramView {
             continue;
         }
             inputs[0] = gamePlayerName;
-               valid = true;
-               
-            }
-            
+            valid = true;          
+        }          
             return inputs;
-            
+           
     }
     
     // This will be changed on the next step
     
     private static boolean doAction(String[] inputs) {
-     System.out.println("**** doAction() called ***");
-     System.out.println("\tinputs = " + inputs[0]);
-     return true;
+        
+           String playersName = inputs[0];
+           Player player = GameControl.savePlayer(playersName);
+           
+           if (player == null) {
+           System.out.println("Could not create the player." + "\n"
+                   + "Enter a different name.");
+           return false;
+           }
+           
+           else {
+           System.out.println("======================================= "
+           + "\nWelcome to the game " + playersName
+           + "\nWe hope you have a lot of fun!"
+           + "\n=======================================");
+           
+           MainMenuView mainMenuView = new MainMenuView();
+           mainMenuView.displayMainMenuView();
+           return true;
+           
+                   
+            }
+
+    }
+
+    private static class GameControl {
+
+        private static Player savePlayer(String playersName) {
+            System.out.println("*****savePlayer() called ***");
+            return new Player();
+            
+        }
+
+        public GameControl() {
+        }
+    }
+
+    private static class MainMenuView {
+
+        
+        public MainMenuView() {
+        }
+
+        private void displayMainMenuView() {
+           System.out.println("*****displayMainMenuView() called ****");
+        }
     }
     
 }
